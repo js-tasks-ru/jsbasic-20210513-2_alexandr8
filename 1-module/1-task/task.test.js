f6a62354171183de8876bc430278e82e9d3ff742
Alexandr Starodubenko <alexvladstr@gmail.com>
2021-05-19 07:45:39 +0300

    Porting additional tests to task-student.test.js

--- 1-module/1-task/task.test.js
+++ 1-module/1-task/task.test.js
@@ -1,44 +1,4 @@
 describe('1-module-1-task', () => {
-
-  // isNumber tests 
-  it('isNumber()  вернёт  false', () => {
-    const values = ['1', NaN, Infinity, -Infinity, true, [], {}];
-    for (val of values) {
-      expect(isNumber(val)).toEqual(false);
-    }
-  });
-
-  it('isNumber()  вернёт  true', () => {
-    const values = [-1, 0, 1.25];
-    for (val of values) {
-      expect(isNumber(val)).toEqual(true);
-    }
-  });
-
-  // sIntegerAndNotNegative tests
-  it('isIntegerAndNotNegative()  вернёт  false', () => {
-    const values = [-1, 1.25, 1.000000000001];
-    for (val of values) {
-      expect(isIntegerAndNotNegative(val)).toEqual(false);
-    }
-  });
-
-  it('isIntegerAndNotNegative()  вернёт  true', () => {
-    const values = [0, 1];
-    for (val of values) {
-      expect(isIntegerAndNotNegative(val)).toEqual(true);
-    }
-  });  
-
- // factorial tests
-  it('факториал -1 вернёт 0', () => {
-    expect(factorial(-1)).toEqual(0);
-  });
-
-  it('факториал 1.25 вернёт 0', () => {
-    expect(factorial(1.25)).toEqual(0);
-  });
-
   it('факториал 0 равен 1', () => {
     expect(factorial(0)).toEqual(1);
   });
