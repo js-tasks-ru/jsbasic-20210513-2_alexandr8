111ff8e371ce0abcd6e23bde542581c23d2ca71e
Alexandr Starodubenko <alexvladstr@gmail.com>
2021-05-25 00:04:21 +0300

    Solution of task 'Spam check'

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
+let spams = ['1xBet', 'XXX'];
+
 function checkSpam(str) {
   // ваш код...
+  let result = false;
+  const lowercaseString = str.toLowerCase();
+
+  for (let i = 0; i < spams.length && !result; i += 1) {
+    result = lowercaseString.includes(spams[i].toLowerCase());
+  }
+
+  return result;
 }
