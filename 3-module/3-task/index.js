e822401985c6e35acc8f54235d29361f77d8b246
Alexandr Starodubenko <alexvladstr@gmail.com>
2021-05-29 14:55:44 +0300

    Solution of task 'Translate the text border-left-width into borderLeftWidth'

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
   // ваш код...
+  let index = 0;
+  let result = '';
+
+  while (index < str.length) {
+    let indexUp = str[index] === '-' && index + 1;
+    result = `${result}${indexUp && str[indexUp].toUpperCase() || str[index]}`
+    index += indexUp && 2 || 1;
+  }
+
+  return result;
 }
